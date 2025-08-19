be401ac3fa323f4fbb9b533cf5da1419ace71e40
SK-20231203RKAA\Administrator <597982448@qq.com>
2025-08-20 03:03:26 +0800

    cesium

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useRef, useEffect } from 'react'
+import { Ion, Viewer } from 'cesium'
+import 'cesium/Build/Cesium/Widgets/widgets.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const cesiumContainerRef = useRef<HTMLDivElement>(null)
+  const viewerRef = useRef<Viewer | null>(null)
 
+  Ion.defaultAccessToken = 
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlNzU1M2Y2ZC03YmYwLTRmNDMtODMzNS0wNzRhNzI0MGUyMjciLCJpZCI6MzMzNDcxLCJpYXQiOjE3NTU2Mjg3NjJ9.guMlkKo5DoTHCEqvLZYFvzt57ND8cra6b-cPhkHEbao';
+
+  useEffect(() => {
+    if (cesiumContainerRef.current) {
+      viewerRef.current = new Viewer(cesiumContainerRef.current);
+    }
+    return () => {
+      if (viewerRef.current) {
+        viewerRef.current.destroy();
+        viewerRef.current = null;
+      }
+    }
+  }, [])
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div ref={cesiumContainerRef} className="w-full h-full">
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import cesium from 'vite-plugin-cesium'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cesium({}), tailwindcss()],
 })
